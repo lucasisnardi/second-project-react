b7aca784adfa438f36ec5f5ad1e2598e5c30161f
lucasisnardi <lucasisnardi08@gmail.com>
2024-03-10 13:37:15 -0300

    atualizado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+
+import axios from "axios";
+import Burger from "./assets/burger 1.svg";
+
+
+
+import {
+  Container,
+  H1,
+  Image,
+  InputLabel,
+  Input,
+  Button,
+  User,
+} from "./styles";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const inputOrder = useRef();
+  const inputclientName = useRef();
+
+  async function addNewUser() {
+
+    const data = await axios.post("http://localhost:3001/users", {
+      pedido: inputOrder.current.value,
+      name: inputclientName.current.value,
+    });
+      console.log(data)
+  //   setUsers([
+  //     ...users,
+  //     {
+  //       id: Math.random(),
+  //       pedido: inputOrder.current.value,
+  //       name: inputclientName.current.value,
+  //     },
+  //   ]);
+ }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Image alt="logo-imagem" src={Burger} />
+
+      <H1>Faça seu pedido!</H1>
+
+      <InputLabel>Pedido</InputLabel>
+      <Input ref={inputOrder} placeholder="1 Coca-Cola, 1-X Salada" />
+
+      <InputLabel>Nome do Cliente</InputLabel>
+      <Input ref={inputclientName} placeholder="Steve Jobs" />
+
+      <Button onClick={addNewUser}>
+
+        Novo Pedido
+      </Button>
+      <ul>
+        {users.map((user) => (
+          <User Key={user.id}>
+            <p>{user.order}</p> <p>{user.clientName}</p> <p>{user.price}</p> <p>{user.status}</p>
+            
+          </User>
+
+        ))}
+      </ul>
+
+    </Container>
   );
 }
+
 
 export default App;
---
+++ src/styles.js
@@ -0,0 +1,84 @@
+import styled from 'styled-components'
+import Background from './assets/background1.svg'
+
+export const Container = styled.div`
+    background: url("${Background}");
+    background-size: cover;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 40px;
+`;
+export const Image = styled.img`
+    margin-top: 30px;
+`;
+
+export const H1 = styled.h1`
+    color: #FFF;
+    text-align: center;
+    font-family: Roboto;
+    font-size: 28px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+`;
+
+export const InputLabel = styled.p`
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 22px;
+    letter-spacing: -0.408px;
+    color: #EEE;
+
+    margin-left: 25px;
+`;
+export const Input = styled.input`
+    border-radius: 14px;
+    background: rgba(255, 255, 255, 0.25);
+
+    width: 342px;
+    height: 58px;
+    padding-left: 25px;
+    margin-bottom: 34px;
+
+    border: none;
+    outline: none;
+
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 300;
+    line-height: normal;
+    color: #FFF;
+`;
+export const Button = styled.button`
+    width: 342px;
+    height: 68px;   
+    display: flex;
+    padding: 0px 113px;
+    justify-content: center;
+    align-items: center;
+    background: #D93856;
+    border: none;
+
+    font-size: 17px;
+    font-style: normal;
+    font-weight: 900;
+    line-height: 2.5px;
+    color: #FFF;
+
+    cursor: pointer;
+
+    &:hover {
+        opacity: 0.8;
+    }
+
+    &:active{
+        opacity: 0.5;
+    }
+`;
+
+export const User = styled.li `
+    
+`
