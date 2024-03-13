8b67ca51b555f8756a690916c943be8302a8f7bb
lucasisnardi <lucasisnardi08@gmail.com>
2024-03-13 19:56:31 -0300

    backup

--- src/containers/Users/index.js
+++ src/containers/Users/index.js
@@ -1,73 +1,49 @@
-import React, { useState, useEffect, useRef } from "react";
-
+// index.js
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-
 import Burger from "../../assets/burger 2.svg";
 import Trash from "../../assets/trash.svg";
-
-
-
-import {
-  Container,
-  H1,
-  Image,
-  Input,
-  InputLabel,
-  Button,
-  User,
-} from "./styles";
+import { Container, H1, Image, Button, User, Status } from "./styles";
 
 function Users() {
   const [users, setUsers] = useState([]);
-  const inputOrder = useRef();
-  const inputclientName = useRef();
-  
+
   useEffect(() => {
     async function fetchUsers() {
-      const { data: newUsers } = await axios.get("http://localhost:3001/users")
-
+      const { data: newUsers } = await axios.get("http://localhost:3001/users");
       setUsers(newUsers);
     }
-    fetchUsers()
+    fetchUsers();
   }, []);
 
-    async function deleteUser(userId) {
-      await axios.delete(`http://localhost:3001/users/${userId}`)
-  
-      const newUsers = users.filter((user) => user.id !== userId);
-  
-      setUsers(newUsers);
-    }
+  async function deleteUser(userId) {
+    await axios.delete(`http://localhost:3001/users/${userId}`);
+    const newUsers = users.filter((user) => user.id !== userId);
+    setUsers(newUsers);
+  }
 
   return (
     <Container>
       <Image alt="logo-imagem" src={Burger} />
-
       <H1>Pedidos</H1>
-      <InputLabel></InputLabel>
-      <Input ref={inputOrder} placeholder="1 Coca-Cola, 1-X Salada Steven Jobs" /> 
-
-      <InputLabel></InputLabel>
-      <Input ref={inputclientName} placeholder="1 Batata Grande, 1 X-Bacon, 
-      2 Coca-Colas Light" />
 
       <ul>
         {users.map((user) => (
-          <User Key={user.id}>
-            <p>{user.order}</p> <p>{user.clientName}</p> <p>{user.price}</p> <p>{user.status}</p>
+          <User key={user.id}>
+            <div>
+              <p>Pedido: {user.order}</p>
+              <p>Cliente: {user.clientName}</p>
+            </div>
+            <Status>{user.status}</Status>
             <button onClick={() => deleteUser(user.id)}>
               <img src={Trash} alt="lata-de-lixo" />
             </button>
           </User>
-
         ))}
       </ul>
-      <Button to="/">
-        Voltar
-     </Button>
+      <Button to="/">Voltar</Button>
     </Container>
   );
 }
-
 
 export default Users;
--- src/containers/Users/styles.js
+++ src/containers/Users/styles.js
@@ -1,90 +1,102 @@
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
-import Background from '../../assets/background.svg'
+// style.js
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import Background from "../../assets/background1.svg";
+
 
 export const Container = styled.div`
-    background: url("${Background}");
-    background-size: cover;
-
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 40px;
+  background: url("${Background}");
+  background-size: cover;
+  background-position: center;
+  background-color: black;
+  width: 100vw;
+  height: 100vh; 
+  height: 1500px;
+  min-height: 100vh; 
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 40px;
 `;
+
 export const Image = styled.img`
-    margin-top: 30px;
+  margin-top: 30px;
 `;
 
 export const H1 = styled.h1`
-    color: #FFF;
-    text-align: center;
-    font-family: Roboto;
-    font-size: 28px;
-    font-style: normal;
-    font-weight: 700;
-    line-height: normal;
-`;
-
-export const InputLabel = styled.p`
-    font-size: 18px;
-    font-style: normal;
-    font-weight: 700;
-    line-height: 22px;
-    letter-spacing: -0.408px;
-    color: #EEE;
-
-    margin-left: 25px;
-`;
-export const Input = styled.input`
-    border-radius: 14px;
-    background: rgba(255, 255, 255, 0.25);
-
-    width: 342px;
-    height: 58px;
-    padding-left: 25px;
-    margin-bottom: 34px;
-
-    border: none;
-    outline: none;
-
-    font-size: 18px;
-    font-style: normal;
-    font-weight: 300;
-    line-height: normal;
-    color: #FFF;
-
-    display: flex;
-    flex-direction: column;
+  color: #fff;
+  text-align: center;
+  font-family: Roboto;
+  font-size: 28px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
 `;
 
 export const Button = styled(Link)`
-    width: 342px;
-    height: 68px;   
-    display: flex;
-    padding: 0px 113px;
-    justify-content: center;
-    align-items: center;
-    background: rgba(255, 255, 255, 0.14);
-    border: none;
-    margin-top: 120px;
+  width: 342px;
+  height: 68px;
+  display: flex;
+  padding: 0px 113px;
+  justify-content: center;
+  align-items: center;
+  background: rgba(255, 255, 255, 0.14);
+  border: none;
+  margin-top: 120px;
+  font-size: 17px;
+  font-style: normal;
+  font-weight: 900;
+  line-height: 2.5px;
+  color: #fff;
+  cursor: pointer;
 
-    font-size: 17px;
-    font-style: normal;
-    font-weight: 900;
-    line-height: 2.5px;
-    color: #FFF;
+  &:hover {
+    opacity: 0.8;
+  }
 
-    cursor: pointer;
-
-    &:hover {
-        opacity: 0.8;
-    }
-
-    &:active{
-        opacity: 0.5;
-    }
+  &:active {
+    opacity: 0.5;
+  }
 `;
 
-export const User = styled.li `
-    
-`
+export const User = styled.li`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 14px;
+  padding: 10px;
+  margin-bottom: 10px;
+  width: 342px;
+  height: auto;
+
+  div {
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+  }
+
+  p {
+    color: #fff;
+    font-size: 16px;
+    margin: 0;
+  }
+
+  button {
+    background: transparent;
+    border: none;
+    cursor: pointer;
+
+    img {
+      width: 20px;
+      height: 20px;
+      object-fit: contain;
+    }
+  }
+`;
+
+export const Status = styled.p`
+  color: #fff;
+  font-size: 16px;
+  margin: 0;
+`;
